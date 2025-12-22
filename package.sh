c1e05cd985320eaf4c5436d7d8c3026c00a86b88
Fei Yang <io@feiyang.eu.org>
2025-12-22 19:19:51 +0800

    Switch repository host

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 cd workdir
-NGINX_VERSION="$(cat ver/NGINX)"
+. ver
+
 cd deb
 sed -i "s/__NGINX_VERSION__/$NGINX_VERSION/" DEBIAN/control
 mkdir -p etc/nginx lib/systemd/system usr/sbin var/lib/nginx var/log/nginx
@@ -11,9 +12,8 @@ find -maxdepth 1 ! -name '.' ! -name 'DEBIAN' -exec chown -R root:root {} \;
 find -type d ! -name 'DEBIAN' -exec chmod 755 {} \;
 chmod 750 var/lib/nginx var/log/nginx
 chmod 755 usr/sbin/nginx
-dpkg-deb --build . ../../dist/nginx.deb
+dpkg-deb --build . "../../dist/nginx-$NGINX_VERSION.deb"
 cd ..
-
 cd rpm
 sed -i "s/__NGINX_VERSION__/$NGINX_VERSION/" package.spec
 cp ../../conf/* .
@@ -23,9 +23,37 @@ rpmbuild \
     -bb \
     --define "_sourcedir $PWD" \
     --define "_rpmdir $PWD/../../dist" \
-    --define '_rpmfilename %%{NAME}.rpm' \
+    --define '_rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm' \
     --define '__brp_strip_static_archive /bin/true' \
     --define '__brp_remove_la_files /bin/true' \
     package.spec
 cd ..
+
 cd ..
+
+if [ -n "$CLOUDSMITH_API_KEY" ]; then
+    cd dist
+
+    DEB="$(find -name '*.deb' -exec basename {} \;)"
+    DEB_IDENTIFIER="$(curl \
+        --header "Content-Sha256: $(sha256sum "$DEB" | cut -d ' ' -f 1)" \
+        --upload-file "$DEB" \
+        "https://$CLOUDSMITH_USERNAME:$CLOUDSMITH_API_KEY@upload.cloudsmith.io/$CLOUDSMITH_REPOSITORY/$DEB" | jq -r '.identifier')"
+    curl \
+        --request POST \
+        --header "Content-Type: application/json"  \
+        --data "{\"distribution\":\"otherdeb/any-version\",\"package_file\":\"$DEB_IDENTIFIER\"}" \
+        "https://$CLOUDSMITH_USERNAME:$CLOUDSMITH_API_KEY@api.cloudsmith.io/v1/packages/$CLOUDSMITH_REPOSITORY/upload/deb/"
+    RPM="$(find -name '*.rpm' -exec basename {} \;)"
+    RPM_IDENTIFIER="$(curl \
+        --header "Content-Sha256: $(sha256sum "$RPM" | cut -d ' ' -f 1)" \
+        --upload-file "$RPM" \
+        "https://$CLOUDSMITH_USERNAME:$CLOUDSMITH_API_KEY@upload.cloudsmith.io/$CLOUDSMITH_REPOSITORY/$RPM" | jq -r '.identifier')"
+    curl \
+        --request POST \
+        --header "Content-Type: application/json"  \
+        --data "{\"distribution\":\"otherrpm/any-version\",\"package_file\":\"$RPM_IDENTIFIER\"}" \
+        "https://$CLOUDSMITH_USERNAME:$CLOUDSMITH_API_KEY@api.cloudsmith.io/v1/packages/$CLOUDSMITH_REPOSITORY/upload/rpm/"
+
+    cd ..
+fi
