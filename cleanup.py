9f053cb2883ef146cbde4874217ae8dda0666c70
Fei Yang <io@feiya.ng>
2026-05-14 14:59:22 +0800

    Cleanup old packages on repository

---
+++ cleanup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import os
+from packaging.version import Version
+import requests
+
+session = requests.Session()
+session.headers.update({"Authorization": "token " + os.environ["CLOUDSMITH_API_KEY"]})
+
+checked_packages = {}
+packages = session.get(
+    "https://api.cloudsmith.io/v1/packages/"
+    + os.environ["CLOUDSMITH_NAMESPACE"]
+    + "/"
+    + os.environ["CLOUDSMITH_REPOSITORY"]
+    + "?page_size=65535"
+).json()
+
+for package in packages:
+    package_identifier = (
+        package["identifiers"]["distro_version"]
+        + "/"
+        + package["identifiers"]["name"]
+        + "/"
+        + package["identifiers"]["architecture"]
+    )
+    package_version = Version(package["version"])
+
+    if (
+        package_identifier in checked_packages
+        and checked_packages[package_identifier] > package_version
+    ):
+        session.delete(
+            "https://api.cloudsmith.io/v1/packages/"
+            + os.environ["CLOUDSMITH_NAMESPACE"]
+            + "/"
+            + os.environ["CLOUDSMITH_REPOSITORY"]
+            + "/"
+            + package["slug_perm"]
+        )
+        print(
+            "Deleted old version "
+            + package["version"]
+            + "of package "
+            + package_identifier
+        )
+    else:
+        checked_packages[package_identifier] = package_version
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -e
 
 apt update
 apt install --yes \
-    curl gettext git gzip jq tar xz-utils \
+    curl gettext git gzip jq python3 python3-packaging python3-requests tar xz-utils \
     autoconf automake cmake libtool musl-tools \
     debhelper dpkg dpkg-dev rpm
+
